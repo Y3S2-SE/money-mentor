b9998a1727df10031bf8c9b236a9bfd77ab3ea05
Thimeth Sathmika <sathmikakb@gmail.com>
2026-03-29 14:15:34 +0530

    feat: update routes to hve new endpoints

--- backend/routes/course.route.js
+++ backend/routes/course.route.js
@@ -2,14 +2,7 @@ import express from 'express';
 import { protect, authorize } from '../middleware/auth.middleware.js';
 import { validate } from '../middleware/validation.middleware.js';
 import { createCourseRules, updateCourseRules, submitCourseRules } from '../validations/course.validation.js';
-import {
-    createCourse,
-    updateCourse,
-    deleteCourse,
-    getAllCourses,
-    getCourseById,
-    submitCourse
-} from '../controllers/course.controller.js';
+import { createCourse, updateCourse, deleteCourse, getAllCourses, getCourseById, submitCourse, getUserPoints } from '../controllers/course.controller.js';
 
 const router = express.Router();
 
@@ -20,6 +13,7 @@ router.put('/:id', authorize('admin'), updateCourseRules, validate, updateCourse
 router.delete('/:id', authorize('admin'), deleteCourse);
 
 router.get('/', getAllCourses);
+router.get('/my-points', getUserPoints);
 router.get('/:id', getCourseById);
 router.post('/:id/submit', submitCourseRules, validate, submitCourse);
 
